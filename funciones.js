6442be18a3ac02d3e5ed6b94d391e38f91150355
JSandoval <jsandoval@iMac-de-Johana.local>
2023-11-06 23:33:17 +0100

    las_lobas_del_norte

---
+++ funciones.js
@@ -0,0 +1,11 @@
+function mostrarSalud() {
+    alert("El estado de salud es óptimo.");
+}
+
+function mostrarEstres() {
+    alert("El nivel es estrés es moderadamente alto.");
+}
+
+function mostrarProgreso() {
+    alert("El progreso se encuentra al 31%");
+}
